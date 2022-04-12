6197e6be8be0a3648fde939368435b0502eb1453
min-kyou <sho9uttot1helo8rd@gmail.com>
2022-04-12 14:44:59 +0900

    スケジュール作成

---
+++ config/schedule.rb
@@ -0,0 +1,38 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Rails.rootを使用するために必要
+require File.expand_path(File.dirname(__FILE__) + "/environment")
+# cronを実行する環境変数
+rails_env = ENV['RAILS_ENV'] || :development
+# cronを実行する環境変数をセット
+set :environment, rails_env
+# cronのログの吐き出し場所
+set :output, "#{Rails.root}/log/cron.log"
+
+# 日曜・木曜の6時に実行
+# every :monday, at: '6:00 am' do
+#   runner "Batch::LineBotBatch.run"
+# end
+#
+# every :thursday, at: '6:00 am' do
+#   runner "Batch::LineBotBatch.run"
+# end
+
+# Learn more: http://github.com/javan/whenever
--- lib/batch/line_bot_batch.rb
+++ lib/batch/line_bot_batch.rb
@@ -15,38 +15,34 @@ class Batch::LineBotBatch
     url = 'https://qiita.com/Qiita/items/b5c1550c969776b65b9b'
     # 引っかかったこと1 res = open(url) だと`initialize': No such file or directory @ rb_sysopen のエラーがでる
     res = URI.open(url)
-
     body = res.read
 
     charset = res.charset
     html = Nokogiri::HTML.parse(body, nil, charset)
 
     target_id = "#personal-public-article-body"
-    result = html.css(target_id)
+    body = html.css(target_id)
 
     num_array = []
-    # もしimgのalt属性にnewを持つ奴がいたら
-    html.css('#personal-public-article-body').each do |node|
-      i = 1
-      node.css('h3 ~ p').each do |node1|
-        #
-        # pp "================"
-        # pp node1.children.css('img')[0]
-        # pp node1.children.css('img')[0].attributes
-        # pp node1.children.css('img')[0].attribute('alt').value == ":new:"
-        # pp i
+    i = 0
+    body.css('h3 ~ p').each do |node1|
 
-        if node1.children.css('img')[0].attribute('alt').value == ":new:"
-          # 番号を記憶
-          num_array.push(i)
-        end
-        i += 1
+      if node1.children.css('img')[0].attribute('alt').value == ":new:"
+        # imgのalt属性にnewを持つ記事の番号を記憶
+        num_array.push(i)
       end
+      i += 1
     end
+    # pp num_array
 
     num_array.each do |num|
-      body = html.css('#personal-public-article-body')
-      
+      # h3タグ以下を取得
+      title_array = body.xpath("//h3")
+      # 記事タイトル
+      article_title = title_array[num].children.css('a')[1].text
+      # 記事url
+      article_url = title_array[num].children.css('a')[1].attribute('href').text
+
     end
   end
 end
