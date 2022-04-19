759c2da156dd8d317cee1ae26cfda164b093f7fc
min-kyou <sho9uttot1helo8rd@gmail.com>
2022-04-19 12:02:17 +0900

    lineで苦戦中

--- lib/batch/line_bot_batch.rb
+++ lib/batch/line_bot_batch.rb
@@ -3,13 +3,15 @@ require 'open-uri'
 require 'json'
 require 'logger'
 require 'nokogiri'
+require 'line/bot'
+Bundler.require
 
 # 引っかかったこと このクラス名はファイル名と合わせないとエラーをはく（あほ）
 class Batch::LineBotBatch
-  # def initialize
-  #   logger = Logger.new('../../../log/api.log')
-  #
-  # end
+  def initialize
+    # logger = Logger.new('../../../log/api.log')
+
+  end
 
   def self.run
     url = 'https://qiita.com/Qiita/items/b5c1550c969776b65b9b'
@@ -23,26 +25,101 @@ class Batch::LineBotBatch
     target_id = "#personal-public-article-body"
     body = html.css(target_id)
 
-    num_array = []
+    # line-botのクライアント作成
+    # client = create_client
+    #
+    client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+
+    # num_array = []
     i = 0
+    # h3タグ以下を取得
+    title_array = body.xpath("//h3")
+
     body.css('h3 ~ p').each do |node1|
 
       if node1.children.css('img')[0].attribute('alt').value == ":new:"
         # imgのalt属性にnewを持つ記事の番号を記憶
-        num_array.push(i)
+        # num_array.push(i)
+
+        # 記事タイトル
+        # article_title = title_array[i].children.css('a')[1].text
+        # 記事url
+        article_url = title_array[i].children.css('a')[1].attribute('href').text
       end
       i += 1
+
+      uri = URI('https://git.heroku.com/blueberry-custard-37486.git/callback')
+      params = {}
+      request = Net::HTTP.post_form(uri, params)
+      # ラインapi
+      body = request.body
+      pp body
+      signature = request.env['HTTP_X_LINE_SIGNATURE']
+      unless client.validate_signature(body, signature)
+        halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
+      end
+      events = client.parse_events_from(body)
+
+      events.each do |event|
+        pp event
+        case event
+        when Line::Bot::Event::Message
+          case event.type
+          when Line::Bot::Event::MessageType::Text
+            # メッセージ作成
+            message = {
+              type: 'text',
+              text: article_url
+            }
+
+            response = client.push_message("<to>", message)
+            pp response
+          end
+        end
+      end
+
+      "OK"
+
     end
     # pp num_array
 
-    num_array.each do |num|
-      # h3タグ以下を取得
-      title_array = body.xpath("//h3")
-      # 記事タイトル
-      article_title = title_array[num].children.css('a')[1].text
-      # 記事url
-      article_url = title_array[num].children.css('a')[1].attribute('href').text
-
-    end
   end
+
+  # def create_client
+  #   @client ||= Line::Bot::Client.new { |config|
+  #     config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+  #     config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  #   }
+  # end
+  #
+  # def callback(article_url)
+  #   body = request.body.read
+  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
+  #   unless client.validate_signature(body, signature)
+  #     halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
+  #   end
+  #   events = client.parse_events_from(body)
+  #
+  #   events.each do |event|
+  #     pp event
+  #     case event
+  #     when Line::Bot::Event::Message
+  #       case event.type
+  #       when Line::Bot::Event::MessageType::Text
+  #         # メッセージ作成
+  #         message = {
+  #           type: 'text',
+  #           text: article_url
+  #         }
+  #
+  #         response = client.push_message("<to>", message)
+  #       end
+  #     end
+  #   end
+  #
+  #   "OK"
+  # end
 end
