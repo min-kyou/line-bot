360f37869c789f3baf417aafa66aabc8790d8d27
min-kyou <sho9uttot1helo8rd@gmail.com>
2022-04-20 17:14:29 +0900

    修正

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -4,35 +4,67 @@ class WebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
   
   def callback
+    url = 'https://qiita.com/Qiita/items/b5c1550c969776b65b9b'
+
+    res = URI.open(url)
+    body = res.read
+
+    charset = res.charset
+    html = Nokogiri::HTML.parse(body, nil, charset)
+
+    target_id = "#personal-public-article-body"
+    body = html.css(target_id)
+
+    i = 0
+
+    # h3タグ以下を取得
+    title_array = body.xpath("//h3")
+
+    # line-apiのクライアント取得
     client = create_client
-    body = request.body
-    pp "===========-body"
-    pp body
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
-    unless client.validate_signature(body, signature)
-      halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
-    end
-    events = client.parse_events_from(body)
 
-    events.each do |event|
-      pp event
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          # メッセージ作成
-          message = {
-            type: 'text',
-            text: article_url
-          }
+    body.css('h3 ~ p').each do |node1|
+      if node1.children.css('img')[0].attribute('alt').value == ":new:"
+        # imgのalt属性にnewを持つ記事の番号を記憶
+        # num_array.push(i)
 
-          response = client.push_message("<to>", message)
-          pp response
+        # 記事タイトル
+        # article_title = title_array[i].children.css('a')[1].text
+        # 記事url
+        article_url = title_array[i].children.css('a')[1].attribute('href').text
+
+        body2 = request.body.read
+        pp "===========-body"
+        pp body2
+        signature = request.env['HTTP_X_LINE_SIGNATURE']
+        unless client.validate_signature(body2, signature)
+          halt 400, {'Content-Type' => 'text/plain'}, 'Bad Request'
         end
+
+        events = client.parse_events_from(body2)
+
+        events.each do |event|
+          pp event
+          case event
+          when Line::Bot::Event::Message
+            case event.type
+            when Line::Bot::Event::MessageType::Text
+              # メッセージ作成
+              message = {
+                type: 'text',
+                text: article_url
+              }
+
+              response = client.push_message("<to>", message)
+              pp response
+            end
+          end
+        end
+
+        "OK"
       end
     end
 
-    "OK"
   end
 
   private 
